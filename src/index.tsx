86c28d2b48ea8ac9c18b55205b4ccc288e53b66f
aarushi-07 <aarushi.t01@gmail.com>
2024-11-29 01:34:59 +0530

    basename update

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router basename='/'>
+    <Router basename='/profile'>
       <Header />
       <Routes >
         <Route path="/" element={<Profile />} />
